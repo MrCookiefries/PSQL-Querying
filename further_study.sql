b62f97d1ce16261fb3cd7428e75f23d3f1aa7c0f
MrCookiefries <cookiefries12@gmail.com>
2021-07-28 22:44:48 -0500

    add the finished files

---
+++ further_study.sql
@@ -0,0 +1,26 @@
+-- Bonus questions
+
+-- FS1. Find the name and rating of the top rated apps in each category, among apps that have been installed at least 50,000 times.
+SELECT category, app_name, rating FROM analytics
+    WHERE (rating, category) IN (
+        SELECT MAX (rating), category FROM analytics
+        WHERE min_installs >= 50000
+        GROUP BY category
+    )
+    ORDER BY category
+;
+
+-- FS2. Find all the apps that have a name similar to “facebook”.
+SELECT * FROM analytics
+    WHERE app_name ILIKE '%facebook%'
+;
+
+-- FS3. Find all the apps that have more than 1 genre.
+SELECT * FROM analytics
+    WHERE array_length (genres, 1) > 1
+;
+
+-- FS4. Find all the apps that have education as one of their genres.
+SELECT * FROM analytics
+    WHERE array_position (genres, 'Education') IS NOT NULL
+;
